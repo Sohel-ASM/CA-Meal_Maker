11363b08f73b96f6cc80c2ef81e7ae83835c0100
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-08 20:48:42 -0400

    getter and setter methods for 'appetizers', 'mains', and 'dish' properties

--- main.js
+++ main.js
@@ -4,5 +4,23 @@ const menu = { // menu object with _courses property in it
         mains: [],
         desserts: []
     },
-
+    // getter and setter methods for _courses's properties
+    get appetizers() {
+        return this._courses.appetizers;
+    },
+    get mains() {
+        return this._courses.mains;
+    },
+    get desserts() {
+        return this._courses.desserts;
+    },
+    set appetizers(appetizer) {
+        this._courses.appetizers = appetizer;
+    },
+    set mains(main) {
+        this._courses.mains = main;
+    },
+    set desserts(dessert) {
+        this._courses.desserts = dessert;
+    },
 };
